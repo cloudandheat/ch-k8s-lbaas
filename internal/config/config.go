4a3a13ef96693db11a2e3dd1568526b224de526b
Jakob Mueller <jakob.mueller@cloudandheat.com>
2023-01-26 10:20:04 +0100

    Remove TODO

--- internal/config/config.go
+++ internal/config/config.go
@@ -165,7 +165,6 @@ func FillKeepalivedConfig(cfg *Keepalived) {
 }
 
 func FillNftablesConfig(cfg *Nftables) {
-	// TODO: Add default values before parsing toml
 	cfg.FilterTableName = "filter"
 	cfg.FilterTableType = "inet"
 	cfg.FilterForwardChainName = "forward"
